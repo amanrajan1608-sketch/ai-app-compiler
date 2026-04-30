3741184f020560d55f90df055953a7294a1d5409
amanrajan1608 <amanrajan1608@gmail.com>
2026-04-30 21:26:19 +0530

    Fixing model name for deployment

--- pipeline/engine.py
+++ pipeline/engine.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
 
 class CompilerEngine:
     async def run(self, user_prompt: str):
